20f7ee1a6cbd22fcb3dd94fcbe6188229fac8bbc
senaboyraz <113339@emmauscollege.nl>
2020-11-25 14:13:15 +0000

    namen toegevoegd
    Signed-off-by: valentijnJB <valentijn.buckmann@icloud.com>
    Signed-off-by: senaboyraz <113339@emmauscollege.nl>

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,8 +1,10 @@
 DROP TABLE IF EXISTS products; 
-CREATE TABLE products (
+CREATE TABLE Games (
   ID SERIAL PRIMARY KEY,
   code VARCHAR(15),
   name VARCHAR(255),
   description TEXT,
   price NUMERIC(10, 2)
+  genre TEXT 
 );
+
