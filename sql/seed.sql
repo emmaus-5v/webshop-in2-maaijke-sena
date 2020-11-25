1c4249a427180b5b718b0c001c0f395582db4279
valentijnJB <valentijn.buckmann@icloud.com>
2020-11-25 14:20:58 +0000

    table uit seed gehaalt
    Signed-off-by: valentijnJB <valentijn.buckmann@icloud.com>

--- sql/seed.sql
+++ sql/seed.sql
@@ -4,13 +4,6 @@
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-ID SERIAL PRIMARY KEY,
-  code VARCHAR(15),
-  name VARCHAR(255),
-  description TEXT,
-  price NUMERIC(10, 2)
-  genre
-  
 INSERT INTO Games VALUES (1,100012, "Grand Theft Auto V", "De veelgeprezen en records brekende open wereld van Grand Theft Auto V op de Playstation 4 (PS4) komt naar next-gen.
 
 Grand Theft Auto V maakt volledig gebruik van de kracht van next gen-spelcomputers door verbeteringen te leveren zoals verhoogde resolutie en grafische details, hogere verkeersdichtheid, weidsere uitzichten, verbeterde AI, de toevoeging van nieuwe wilde dieren en geavanceerde weer- en schade-effecten voor een ultieme open wereld-ervaring."15);
