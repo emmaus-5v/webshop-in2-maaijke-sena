d110c4e7792b26b4b27704ce34a9ecc4089ad6b4
senaboyraz <113339@emmauscollege.nl>
2020-12-02 12:51:07 +0000

    xx
    Signed-off-by: senaboyraz <113339@emmauscollege.nl>

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,51 +1,46 @@
 DROP TABLE IF EXISTS products; 
 CREATE TABLE Games (
-<<<<<<< HEAD
-  id INTEGER PRIMARY KEY,
-  code INTEGER,
-  name TEXT,
-=======
   ID INTEGER PRIMARY KEY,
-  code VARCHAR(15),
-  name VARCHAR(255),
->>>>>>> 7f71faedffe4c6b9e467e79518f616f20148e146
+  code INTEGER(15),
+  name TEXT(255),
   description TEXT,
   genre TEXT,
   uitgever TEXT,
   releasedatum TEXT,
-  price INTEGER); 
+  price INTEGER
+);
 
 CREATE TABLE horror Games (
-  ID SERIAL PRIMARY KEY,
-  code VARCHAR,
-  name VARCHAR
+  ID INTEGER PRIMARY KEY,
+  code INTEGER,
+  name TEXT,
   description TEXT,
   era INTEGER,
   intensity level TEXT,
   age INTEGER,
-  price NUMERIC,
+  price INTEGER,
  );
 
  CREATE TABLE action Games (
-   ID SERIAL PRIMARY KEY,
-   code VARCHAR,
-   name VARCHAR,
+   ID INTEGER PRIMARY KEY,
+   code INTEGER,
+   name TEXT,
    description TEXT,
    era INTEGER,
    circumstance TEXT,
    intensity level TEXT,
    age INTEGER,
-   price NUMERIC
+   price INTEGER
 );
 
 CREATE TABLE RPG Games (
-  ID SERIAL PRIMARY KEY,
-  code VARCHAR,
-  name VARCHAR,
+  ID INTEGER PRIMARY KEY,
+  code INTEGER,
+  name TEXT,
   description TEXT,
   era INTEGER,
   circumstance TEXT,
   character TEXT,
   age INTEGER,
-  price NUMERIC
+  price INTEGER
 );
